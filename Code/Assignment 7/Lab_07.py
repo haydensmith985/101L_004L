a3b2472927bf1a3206ee8b62c8a1c40dcaf13a35
haydensmith985 <91220819+haydensmith985@users.noreply.github.com>
2021-10-12 00:49:52 -0500

    Cypher Lab
    cypher lab cs101l class

--- Code/Assignment 7/Lab_07.py
+++ Code/Assignment 7/Lab_07.py
@@ -1,51 +1,57 @@
-def encode(shift, string):
-    string = string.upper()
-    encrypted_string = ''
-    for char in string:
-        ord_val = ord(char)
-        char_pos = ord_val - ord("A")
-        new_char_pos = (char_pos + shift) % 26
-        new_char = chr(new_char_pos)
-        encrypted_string = encrypted_string + new_char
-    return encrypted_string
+#Hayden Smith
+#CS101L_004 Cypher Lab
+#This lab will ask the user for a string to encrypt/decrypt and a shift amount
+#The program will convert the string into its unicode position and increase/decrease its position based on the shift value
+#The program will create a new string with the new shifted characters and output the result
+#The program will only accept value answers for the menu choice
+#The program will only accept valid input for a string
 
-def decode(shift, string):
-    string = string.upper()
-    decrypted_string = ''
-    for char in string:
-        ord_val = ord(char)
-        char_pos = ord_val - ord("A")
-        new_char_pos = (char_pos - shift) % 26
-        new_char = chr(new_char_pos)
-        decrypted_string = decrypted_string + new_char
-    return decrypted_string
-def print_menu():
-    print('Welcome to the string encoder! Please select an option: ')
-    print('A) Encode a string')
-    print('B) Decode a string')
-    print('C) Quit program')
+def encrypt(text, shift): #encryption function
+    text.upper()
+    final = ''
+    for c in text:
+        c_ascii = ord(c)
+        c_place = c_ascii - ord('A')
+        c_place_new = (c_place + shift) % 26
+        new_ascii = c_place_new + ord('A')
+        c_final = chr(new_ascii)
+        final += c_final
+    return final
 
+def decrypt(text, shift): #decryption function
+    text.upper()
+    final = ''
+    for c in text:
+        c_ascii = ord(c)
+        c_place = c_ascii - ord('A')
+        c_place_new = (c_place - shift) % 26
+        new_ascii = c_place_new + ord('A')
+        c_final = chr(new_ascii)
+        final += c_final
+    return final
 
+def print_menu(): #menu print function
+    print('Welcome to the string encryptor!')
+    print('Please choose from the following options:')
+    print('A) Encrypt a string')
+    print('B) Decrypt a string')
+    print('C) Quit')
 
-print_menu()
 x = 1
-while x == 1:
-    menu_selection = input()
-    menu_selection = menu_selection.upper()
-    if menu_selection == 'A':
-        user_string = input('Enter a string to encode : \n')
-        user_shift = int(input('Enter a shift value : \n'))
-        print(user_string, 'shifted', user_shift, 'characters looks like:')
-        print(encode(user_shift, user_string))
-    elif menu_selection == 'B':
-        user_string = input('Enter a string to encode : \n')
-        user_shift = int(input('Enter a shift value : \n'))
-        print(user_string, 'shifted', user_shift, 'characters looks like:')
-        print(decode(user_shift, user_string))
-    elif menu_selection == 'C':
+while x == 1: #Functional code loop
+    print_menu()
+    user_choice = input('Selection : ')
+    if user_choice == 'a' or user_choice == 'A':
+        text = input('Enter a string to encrypt : \n')
+        shift = int(input('Enter a shift amount : \n'))
+        print('Encrypted string:', encrypt(text, shift), '\n\n')
+    elif user_choice == 'b' or user_choice == 'B':
+        text = input('Enter a string to decrypt : \n')
+        shift = int(input('Enter a shift amount : \n'))
+        print('Decrypted string:', decrypt(text, shift), '\n\n')
+    elif user_choice == 'c' or user_choice == 'C':
+        print('Thank you. Have a nice day!')
         x = 0
-        print('Thank you for using the string encoder! Have a good day.')
     else:
-        print('The input given did not match the menu options. Please try again.')
-        print_menu()
-
+        print('Your selection is not valid. Please try again.\n\n')
+        continue
