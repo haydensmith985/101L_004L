7dd0d93dd888f29a85472e32b138473c68b3e5a1
haydensmith985 <91220819+haydensmith985@users.noreply.github.com>
2021-11-17 10:14:35 -0600

    Clock update

--- Code/Assignment 12/Clock_Class.py
+++ Code/Assignment 12/Clock_Class.py
@@ -1,24 +1,24 @@
 class Clock():
     def __init__(self, hours=0, minutes=0, seconds=0, type=1):
-        self.hours = hours
+        self.hours = hours #setting object values to passed args
         self.minutes = minutes
         self.seconds = seconds
         self.type = type
 
-    def __str__(self):
+    def __str__(self): #printing clock info based on clock type
         if self.type == 0:
-            return ("{:02}:{:02}:{:02}".format(self.hours, self.minutes, self.seconds))
+            return ("{:02}:{:02}:{:02}".format(self.hours, self.minutes, self.seconds)) #24 hour clock
         elif self.type == 1:
-            if self.hours == 0:
+            if self.hours == 0: #12 hour clock in am
                 return ("{:02}:{:02}:{:02} am".format(self.hours, self.minutes, self.seconds))
             if self.hours > 0 and self.hours <= 12:
                 return ("{:02}:{:02}:{:02} am".format(self.hours, self.minutes, self.seconds))
             if self.hours > 12:
-                self.hours -= 12
+                self.hours -= 12 #12 hour clock in pm
                 return ("{:02}:{:02}:{:02} pm".format(self.hours, self.minutes, self.seconds))
         
-    def tick(self):
-        if self.seconds < 59:
+    def tick(self): #adding one second to clock time
+        if self.seconds < 59: #if elif ladder associated with clock time overfall
             self.seconds += 1
         elif self.seconds == 59:
             self.seconds = 0
--- Code/Assignment 12/Clock_Test.py
+++ Code/Assignment 12/Clock_Test.py
@@ -1,13 +1,13 @@
 from Clock_Class import *
 import time
 
-uhours = int(input('Number of hours: '))
+uhours = int(input('Number of hours: ')) #asking user for input
 uminutes = int(input('Number of minutes: '))
 useconds = int(input('Number of seconds: '))
 
-clock = Clock(uhours, uminutes, useconds)
+clock = Clock(uhours, uminutes, useconds) #creating a clock object
 
-while True:
+while True: #adjusting clock values for seconds passed
     print(clock)
     clock.tick()
     time.sleep(1)
